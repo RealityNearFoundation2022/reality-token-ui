eaa4c998094fb0f9ab1203ecc4cc396055cc0624
Jonathan Andres Cardenas Pabon <card800398@siigo.com>
2023-06-16 17:07:53 -0500

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,53 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import inject from '@rollup/plugin-inject'
-
-import GlobalPolyFill from "@esbuild-plugins/node-globals-polyfill";
-
-import { resolve } from "path";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: 'window',
-    'process.env': {}
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-        define: {
-            global: "globalThis",
-        },
-        plugins: [
-            GlobalPolyFill({
-                process: true,
-                buffer: true,
-            }),
-        ],
-    },
-  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      process: "process/browser",
-      stream: "stream-browserify",
-      zlib: "browserify-zlib",
-      util: "util",
-      Buffer: 'buffer', // Redirige todas las importaciones de "Buffer" a "buffer" 
-    },
-  },
-  build: {
-    sourcemap: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    rollupOptions: {
-			plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
-      external: ['Buffer'],
-		},
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
