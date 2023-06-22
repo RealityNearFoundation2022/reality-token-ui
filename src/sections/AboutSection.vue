6e042f84eb9285d3d26900cd1b1722fa3c952317
yud.cumba <yuditd.cumba@gmail.com>
2023-06-22 16:00:53 -0500

    feat: add responsive

--- src/sections/AboutSection.vue
+++ src/sections/AboutSection.vue
@@ -1,8 +1,8 @@
 <template>
     <v-row id="description">
         <v-col cols="12" sm="6" order="2" order-sm="1" class="pa-sm-12 pcol-2">
-            <h1 class="mb-2">REALITY NEAR</h1>
-            <h5>Welcome to the Reality Near, where you will experience your realities in a whole new way! Our
+            <h1 class="mb-2 mx-2">REALITY NEAR</h1>
+            <p class="mx-2"> Welcome to the Reality Near, where you will experience your realities in a whole new way! Our
                 cutting-edge technology combines the power of augmented and virtual reality with real-life experiences to
                 create an immersive world of endless possibilities. At the heart of our mission is a commitment to
                 facilitating educational and economic growth for our users by reducing physical barriers. Whether you're a
@@ -10,7 +10,7 @@
                 someone who loves to connect with others, Reality Near has something for you!
                 Reality ($RLTS) is the token and main component in the economy of Reality Near. Realities provides fast
                 transaction times and low fees thanks to NEAR protocol, making it a practical choice for our users. Don't
-                miss out on the chance to be a part of the future of reality and buy your Realities ($RLTS) today!</h5>
+                miss out on the chance to be a part of the future of reality and buy your Realities ($RLTS) today!</p>
             <div class="mt-7 d-flex">
                 <v-btn rounded="xl" color="red" class="mr-2 btn-xl mb-2" @click="scrollToSection('#purchase')">SWAP
                     REALITIES</v-btn>
